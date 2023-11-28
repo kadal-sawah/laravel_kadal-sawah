f729520537edaca45ea436d3a0dbb8afb7de801a
kadal-sawah <riz.idn.dev@gmail.com>
2023-11-29 01:40:52 +0700

    add pasien crud & fix delete issue

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\BerandaController;
 use App\Http\Controllers\RsController;
+use App\Http\Controllers\PasienController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,14 +20,26 @@ use App\Http\Controllers\RsController;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// User
 Route::get('/daftar', [UserController::class, 'daftar']);
 Route::post('/daftar', [UserController::class, 'daftarbaru']);
 Route::post('/masuk', [UserController::class, 'login']);
 
+// Dummy
 Route::get('/beranda', [BerandaController::class, 'beranda']);
 
+// Rumah Sakit
 Route::get('/rs',[RsController::class,'view']);
 Route::post('/rs', [RsController::class, 'store']);
 Route::get('/rs/{rs}/edit', [RsController::class, 'edit']);
 Route::put('/rs/{rs}', [RsController::class, 'doupdate']);
-Route::delete('/deleters/{rs}', [RsController::class,'delete']);
+Route::delete('/rs/{rs}/delete', [RsController::class,'delete']);
+
+// Pasien
+Route::get('/pasien', [PasienController::class, 'view']);
+Route::get('/pasien/form', [PasienController::class, 'showform']);
+Route::post('/pasien', [PasienController::class, 'store']);
+Route::get('/pasien/{pasien}/edit', [PasienController::class, 'edit']);
+Route::put('/pasien/{pasien}', [PasienController::class, 'doupdate']);
+Route::delete('/pasien/{pasien}/delete', [PasienController::class, 'delete']);
