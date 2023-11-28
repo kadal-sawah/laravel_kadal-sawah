4a7c96f9ca2a0b407095833292835e50cf3995cc
kadal-sawah <riz.idn.dev@gmail.com>
2023-11-28 23:33:16 +0700

    added fucntion view rs

--- app/Http/Controllers/RsController.php
+++ app/Http/Controllers/RsController.php
@@ -9,6 +9,12 @@ use Illuminate\Validation\Rule;
 
 class RsController extends Controller
 {
+
+    public function view(){
+        $data['rumah_sakit'] = Rs::orderBy('id','desc')->paginate(10);
+        return view ('rs.list', $data);
+    }
+
     public function store(Request $request){
         // return dd($request);
         $inFields = $request->validate([
@@ -25,6 +31,27 @@ class RsController extends Controller
         $inFields['user_id'] = auth()->id();
 
         $rsBaru = Rs::create($inFields);
+        return redirect("/rs");
+    }
+
+    public function edit(Rs $rs){
+        return view('rs.edit-rs',['rs' => $rs]);
+    }
+
+    public function doupdate(Rs $rs, Request $request){
+        $inFields = $request->validate([
+            'namaRs' => 'required',
+            'alamat' => 'required',
+            'email' => 'required',
+            'telepon' => 'required',
+        ]);
+
+        $inFields['nama_rs'] = $inFields['namaRs'];
+        $inFields['alamat'] = $inFields['alamat'];
+        $inFields['email'] = $inFields['email'];
+        $inFields['telepon'] = $inFields['telepon'];
+
+        $rs->update($inFields);
         return redirect("/rs");
     }
 
